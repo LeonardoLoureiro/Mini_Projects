aabfc5a190ec9c6d93341008660930aa083a2c00
Leonardo <Leonardo.K.Loureiro@outlook.com>
2018-11-08 17:54:04 +0000

    In MenuCalc program, user is no able to just add food and price using CSV format inside MenuCalc.txt file.

--- Programs/MenuCalc.py
+++ Programs/MenuCalc.py
@@ -1,20 +1,32 @@
 #!/usr/bin/env python
 # A small program that is used to calculate orders given to waiter. Each item/food/drink will be assigned
-# a number and the waiter will enter orders such as 553 translate to 2 small drinks and 1 salad. 
-# The program will also calculate a proposed tip... 
-# The program also displays the number of each item/food/drink that is ordered
+# a number and the waiter will enter orders.
+# The program will also calculate a proposed tip...
+# The program also displays the number of each item/food/drink that is ordered.
+# Make sure to seperate order numbers with ',' when program asks for them.
+import csv
+'''
+TO BE MADE:
+    import csv
+reader = csv.reader(open('filename.csv', 'r'))
+d = {}
+for row in reader:
+   k, v = row
+   d[k] = v
+'''
 
+menu = {}
 
-menu = {"Chicken w/ rice": 3.99, 
-        "Burger w/ fries": 4.99,
-        "Hot dog": 1.49, 
-        "Potato salad": 3.50,
-        "Small drink": 0.99, 
-        "Medium drink": 1.50,
-        "Large drink": 2, 
-        "Milkshake": 2.49,
-        "Small pasta": 2.50, 
-        "Big pasta": 3.00}
+# Converting from csv and adding it to the menu dict,i but First
+# converting all prices from strings to floats. I could use straight
+# from the DictReader but that would require code edit for the rest
+# of the file which is too long.. :/
+with open('MenuCalc.txt', 'r') as file:
+    reader = csv.DictReader( file, delimiter=';' )
+
+    for item in reader: 
+        k, v = item['FOOD'], float( item['PRICE'] )
+        menu[k] = v
 
 def get_price( order_n ):
     return menu.values()[ order_n - 1 ]
@@ -26,8 +38,12 @@ for item in menu:
     print("%d) %.2f - %s" % ( index, menu[item], item ) )
     index += 1
 
-# Taking orders from user...
-print("\nPlease enter order:\t(separate numbers with \',\')")
+# Taking orders from user and separating them into seperate numbers for
+# the program to be able to 'understand' what the orders are...
+# First the program iterates over the enterire order given then appends
+# each number to a buffer until it encounters a comma, which then the 
+# number so far is converted into an int.
+print("\nPlease enter order:\t(please separate numbers with \',\')")
 order = str(raw_input("> "))
 
 sum = 0.00
@@ -55,7 +71,8 @@ for char in order:
     except:
         continue
 
-# To compensate for the last order since for loop won't iterate once more..
+# To compensate for the last order since for loop won't iterate once more
+# and allow for order to be converted into int and added to sum and list...
 order_n = int( obj )
 sum += get_price( order_n )
 if order_n not in order_repeats:
@@ -64,7 +81,9 @@ if order_n not in order_repeats:
 elif order_n in order_repeats:
     order_repeats[ order_n ] += 1
 
-# Here you display how many number of a certain item on menu is ordered...
+# Here you display how many number of a certain item on menu is ordered
+# along with the frequency of each food order as well as showing the
+# total sum of the bill...
 print("\n" + "-"*8 + " ORDER " + "-"*8 )
 
 for order_item in order_repeats:
