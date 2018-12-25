97f58bc18717ca84b12dbdd5f078ecfd44de8318
Leonardo <Leonardo.K.Loureiro@outlook.com>
2018-12-25 13:10:27 +0000

    cleaned up the code a bit.

--- Programs/MenuCalc.py
+++ Programs/MenuCalc.py
@@ -5,17 +5,8 @@
 # The program also displays the number of each item/food/drink that is ordered.
 # Make sure to seperate order numbers with ',' when program asks for them.
 import csv
-'''
-TO BE MADE:
-    import csv
-reader = csv.reader(open('filename.csv', 'r'))
-d = {}
-for row in reader:
-   k, v = row
-   d[k] = v
-'''
 
-menu = {}
+menu_prices = {}
 
 # Converting from csv and adding it to the menu dict,i but First
 # converting all prices from strings to floats. I could use straight
@@ -26,17 +17,40 @@ with open('MenuCalc.txt', 'r') as file:
 
     for item in reader: 
         k, v = item['FOOD'], float( item['PRICE'] )
-        menu[k] = v
+        menu_prices[k] = v
+
 
 def get_price( order_n ):
-    return menu.values()[ order_n - 1 ]
+    return menu_prices.values()[ order_n - 1 ]
 
-# Display food on the menu for user...
-print("\n" + "-"*8 + " MENU " + "-"*9 )
-index = 1
-for item in menu:
-    print("%d) %.2f - %s" % ( index, menu[item], item ) )
-    index += 1
+
+def show_order( order_repeats ):
+    print("\n" + "-"*8 + " ORDER " + "-"*8 )
+    
+    for order_item in order_repeats:
+        print("%d x %s" % (order_repeats[order_item], 
+                           menu_prices.keys()[ int( order_item - 1 ) ]))
+
+
+def show_sum( sum ):
+    print("\n" + "-"*9 + " SUM " + "-"*9 )
+    print("Sum: %.2f" % sum)
+    print("Sum with recommended tip (20%%): %.2f" % (sum*1.2) )
+
+
+def display_menu():
+    print("\n" + "-"*8 + " MENU " + "-"*9 )
+    index = 1
+    for item in menu_prices:
+        print("%d) %.2f - %s" % ( index, 
+                                  menu_prices[item], 
+                                  item ) )
+        index += 1
+
+
+# ==========================================================================
+#
+display_menu()
 
 # Taking orders from user and separating them into seperate numbers for
 # the program to be able to 'understand' what the orders are...
@@ -45,22 +59,22 @@ for item in menu:
 # number so far is converted into an int.
 print("\nPlease enter order:\t(please separate numbers with \',\')")
 order = str(raw_input("> "))
+order += ","    # so last food item can be processed.
 
 sum = 0.00
 order_repeats = {}
 
-obj = ''
+food_item = ''
 for char in order:
     if char != ',':
-        obj += char
+        food_item += char
         continue
 
-    order_n = int( obj )
+    order_n = int( food_item )
     
     try:
         sum += get_price( order_n )
-
-        obj = ''
+        food_item = ''
 
         if order_n not in order_repeats:
             order_repeats[ order_n ] = 1
@@ -71,25 +85,8 @@ for char in order:
     except:
         continue
 
-# To compensate for the last order since for loop won't iterate once more
-# and allow for order to be converted into int and added to sum and list...
-order_n = int( obj )
-sum += get_price( order_n )
-if order_n not in order_repeats:
-    order_repeats[ order_n ] = 1
-
-elif order_n in order_repeats:
-    order_repeats[ order_n ] += 1
-
 # Here you display how many number of a certain item on menu is ordered
 # along with the frequency of each food order as well as showing the
 # total sum of the bill...
-print("\n" + "-"*8 + " ORDER " + "-"*8 )
-
-for order_item in order_repeats:
-    print("%d x %s" % (order_repeats[order_item],
-                       menu.keys()[ int( order_item - 1 ) ] ) )
-
-print("\n" + "-"*9 + " SUM " + "-"*9 )
-print("Sum: %.2f" % sum)
-print("Sum with recommended tip (20%%): %.2f" % (sum*1.2) )
+show_order(order_repeats)
+show_sum(sum)
